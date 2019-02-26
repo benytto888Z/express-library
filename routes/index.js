db92dfe7c231d0aa79e6a705570b6e53196ed9e9
benytto888Z <benamos111@gmail.com>
2019-02-26 01:48:09 +0100

    First version of application moved into github

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ res.redirect('/catalog');
+
+ //res.render('index', { title:'Hello there!' })
+});
+
+module.exports = router;
